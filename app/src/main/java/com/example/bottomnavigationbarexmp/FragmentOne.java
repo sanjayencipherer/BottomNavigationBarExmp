1882e56d26a637d04486725a972fa7c135b62608
Sanjayjith <sanjayjith.m@photoninfotech.net>
2022-05-19 12:32:35 +0530

    Initial Commit - Created Bottom Navigation Bar Example

---
+++ app/src/main/java/com/example/bottomnavigationbarexmp/FragmentOne.java
@@ -0,0 +1,14 @@
+package com.example.bottomnavigationbarexmp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+class FragmentOne extends Fragment {
+
+   @Override
+   public void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+   }
+}
--- app/src/main/java/com/example/bottomnavigationbarexmp/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationbarexmp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.bottomnavigationbarexmp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -12,26 +15,38 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView helloWorldTV;
+    //private TextView helloWorldTV;
     private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        helloWorldTV = findViewById(R.id.helloWorldTV);
+        //helloWorldTV = findViewById(R.id.helloWorldTV);
+        frameLayout = findViewById(R.id.frameLayout);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.action_search)
-                    helloWorldTV.setText(R.string.menu_search);
+                if(item.getItemId() == R.id.action_search) {
+                    //helloWorldTV.setText(R.string.menu_search);
+                    Log.d("Nothing", "Nothing");
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .add(R.id.frameLayout, FragmentOne.class, null)
+                            .setReorderingAllowed(true)
+                            .addToBackStack(null)
+                            .commit();
+                }
                 if (item.getItemId() == R.id.action_settings)
-                    helloWorldTV.setText(R.string.menu_settings);
+                    //helloWorldTV.setText(R.string.menu_settings);
+                    Log.d("Nothing", "Nothing");
                 if (item.getItemId() == R.id.action_navigation)
-                    helloWorldTV.setText(R.string.menu_navigation);
+                    //helloWorldTV.setText(R.string.menu_navigation);
+                    Log.d("Nothing", "Nothing");
 
                 return true;
             }
